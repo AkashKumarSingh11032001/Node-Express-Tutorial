4da2925ccf7b7b4f344475635430134f535fb751
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-13 18:53:01 +0530

    basis init

---
+++ Node/app.js
@@ -0,0 +1,31 @@
+// I -Intro
+
+// const amount = 12;
+// if(amount < 10) {
+//     console.log("Small");
+// }
+// else{
+//     console.log("Large");
+// }
+
+// console.log("First node app")
+
+// 2- setInterval
+
+// setInterval(() => {
+//     console.log("Hello World")
+// }, 1000);
+
+// 3- Modules
+
+// const sayHi = (name) => {
+//     console.log(`Hello ${name}`);
+// }
+
+// sayHi("Akash");
+// sayHi("Singh")
+
+// exort and import
+
+// export syntax => module.exports = {/* required para *\}  //export can be any things
+// import syntax => const xyz = require('path-of-module') // remember that 'xyz' is an object
